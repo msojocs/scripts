631521b8b1e6cb5769a7e0338e9f9f2c0473b480
msojocs <jiyecafe@gmail.com>
2024-05-21 14:04:50 +0800

    perf: alpine-loongarch

--- alpine-loongarch/build.sh
+++ alpine-loongarch/build.sh
@@ -4,4 +4,6 @@ version="240514"
 wget -c https://dev.alpinelinux.org/~loongarch/edge/releases/loongarch64/alpine-minirootfs-edge-${version}-loongarch64.tar.gz
 
 # https://docs.docker.com/build/building/base-images/
-docker import alpine-minirootfs-*.tar.gz "alpine:edge-${version}"
+docker import alpine-minirootfs-*.tar.gz "alpine:3.20"
+
+docker run --rm -it "alpine:3.20" /bin/sh
